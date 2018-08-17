236a7a96d9a368efbfca1178f8ceaaa29ca1585d
SSK0206 <coccinelle.sasa@gmail.com>
2018-08-17 15:54:04 +0900

    アイコンを表示させる

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @user = current_user
+    @posts = @user.posts.all
   end
 
   # GET /posts/1
