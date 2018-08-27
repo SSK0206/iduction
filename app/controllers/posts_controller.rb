ce5cd7184c7329db8075b33c59aca132cfbb94c7
SSK0206 <coccinelle.sasa@gmail.com>
2018-08-27 12:27:20 +0900

    Change redirect_to url

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,9 +62,10 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to posts_url, notice: '削除されました' }
-      format.json { head :no_content }
+        format.html { redirect_to user_url, notice: '削除されました' }
+        format.json { head :no_content }
     end
+    
   end
 
   private
