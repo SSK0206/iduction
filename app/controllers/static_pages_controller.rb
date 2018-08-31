01794e9508f295b461d69b0a00edb010c494e0cd
SSK0206 <coccinelle.sasa@gmail.com>
2018-08-31 19:55:43 +0900

    Add like_ranking

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,7 @@ class StaticPagesController < ApplicationController
   def home
   	@user = current_user if user_signed_in?
   	@posts = Post.all
+  	@rank = Post.order('likes_count Desc').limit(5)
   end
 
   def help
