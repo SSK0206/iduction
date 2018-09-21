ba7370d424cc1827dcfa0854d4bc46858ce7de33
SSK0206 <coccinelle.sasa@gmail.com>
2018-09-21 16:06:22 +0900

    Change the background color

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -62,8 +62,8 @@ class PostsController < ApplicationController
   def destroy
     @post.destroy
     respond_to do |format|
-        format.html { redirect_to user_url, notice: '削除されました' }
-        format.json { head :no_content }
+        format.html { redirect_to root_url, notice: '削除されました' }
+        # format.json { head :no_content }
     end
     
   end
